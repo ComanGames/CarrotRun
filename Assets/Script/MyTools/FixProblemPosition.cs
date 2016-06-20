20c789947d59d781944b3efcbb9a7c8a5e9b92de
ComanGames <ComanGames@WORKSTATION>
2016-06-20 04:25:30 +0000

    New changes

---
+++ Assets/Script/MyTools/FixProblemPosition.cs
@@ -0,0 +1,53 @@
+﻿using Assets.Script.GamePlay;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Script.MyTools
+{
+    public class FixProblemPosition
+    {
+
+        public const float TopDistance = -0.03396f;
+        public const float CoinPosition = 4.3f;
+        [MenuItem("MyTools/FixCoins")]
+        public static void FixingCoins()
+        {
+            Transform[] transforms = Selection.transforms;
+            foreach (Transform transform in transforms)
+            {
+                FixConPositionChildes(transform);
+            }
+        }
+
+        private static void FixConPositionChildes(Transform transform)
+        {
+            foreach (Transform transformChild in transform)
+            {
+                if (transformChild.GetComponentInChildren<Coin>() != null)
+                {
+                   Transform ourCoinTransform = transformChild.GetChild(0);
+                   ourCoinTransform.localPosition = new Vector3(0,CoinPosition);
+                }
+            }
+        }
+
+
+        [MenuItem("MyTools/FixProblem")]
+        public static void FixingPosition()
+        {
+            Transform[] transforms = Selection.transforms;
+            foreach (Transform transform in transforms)
+            {
+                FixPositionForChildes(transform);
+            }
+        }
+
+        private static void FixPositionForChildes(Transform transform)
+        {
+            foreach (Transform transformChild in transform)
+            {
+                transformChild.localPosition = new Vector3(transformChild.localPosition.x,transformChild.localPosition.x*TopDistance);
+            }
+        }
+    }
+}
